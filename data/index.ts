eea3f306827c53451c952a0f761133cc1465465b
misakii <misakiigmd@gmail.com>
2024-08-10 14:08:27 +0200

    Added Sentry + Clients

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,8 @@
 export const navItems = [
+  { name: "Home", link: "#home" },
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  /* { name: "Testimonials", link: "#testimonials" }, */
   { name: "Contact", link: "#contact" },
 ];
 
