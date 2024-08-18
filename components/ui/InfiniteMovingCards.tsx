183029907f8ecf2c8ef4fcf8354a8d6b41f4a48c
misakii <misakiigmd@gmail.com>
2024-08-18 16:42:33 +0200

    Added profile as an item for the moving cards

--- components/ui/InfiniteMovingCards.tsx
+++ components/ui/InfiniteMovingCards.tsx
@@ -14,6 +14,7 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
+    profile: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -113,7 +114,7 @@ export const InfiniteMovingCards = ({
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <div className="me-3">
-                  <img src="/profile.svg" alt="profile" />
+                  <img src={item.profile} alt="profile" />
                 </div>
                 <span className="flex flex-col gap-1">
                   <span className="text-xl font-bold leading-[1.6] text-white">
